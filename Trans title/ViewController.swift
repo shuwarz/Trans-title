5dd7e44fcf70a4741949ee86e1834a57219398d1
shuwarz <imatakakun@gmail.com>
2021-04-30 19:27:43 +0900

    Initial Commit

---
+++ Trans title/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trans title
+//
+//  Created by 今泉　孝陽 on 2021/04/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
