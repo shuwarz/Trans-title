eac3c77d6ca4e0a9f772c652c08797612e29719c
Tomoya Tanaka <tomoya113.code@gmail.com>
2021-10-29 17:37:21 +0900

    [fix] bug

--- Trans title/AppDelegate.swift
+++ Trans title/AppDelegate.swift
@@ -12,8 +12,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-		let appDomain = Bundle.main.bundleIdentifier
-		UserDefaults.standard.removePersistentDomain(forName: appDomain!)
         return true
     }
 
